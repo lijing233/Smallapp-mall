739e9a078e97d1864612f37b610ba8a06e8015e4
zhouyali <zhouyali@gomeholdings.com>
2018-03-22 19:06:58 +0800

    复选框完成

--- demo/pages/cart/cart.js
+++ demo/pages/cart/cart.js
@@ -5,88 +5,72 @@ Page({
    */
   data: {
     list: [{
-      id:"1","shopname": "商城abc", "isChecked": false, "products": [
-        { "isChecked": false, "name": "盆栽花四季播种单品单束", "spec": "粉，鲜花种子", "unit": "30粒/包", "price": "$18.00", "num": "3", id: "01" },
-        { "isChecked": false, "name": "盆栽花四季播种单品单束", "spec": "粉，鲜花种子", "unit": "30粒/包", "price": "$18.00", "num": "3", id: "02" },
-        { "isChecked": false, "name": "盆栽花四季播种单品单束", "spec": "粉，鲜花种子", "unit": "30粒/包", "price": "18.00", "num": "3", id: "03" }
+      id:"1","shopname": "商城abc", "isChecked": true, "products": [
+        { "isChecked": true, "name": "盆栽花四季播种单品单束", "spec": "粉，鲜花种子", "unit": "30粒/包", "price": "$18.00", "num": "3", id: "01" },
+        { "isChecked": true, "name": "盆栽花四季播种单品单束", "spec": "粉，鲜花种子", "unit": "30粒/包", "price": "$18.00", "num": "3", id: "02" },
+        { "isChecked": true, "name": "盆栽花四季播种单品单束", "spec": "粉，鲜花种子", "unit": "30粒/包", "price": "18.00", "num": "3", id: "03" }
       ]
     }],
-    isChecked: false,
+    isChecked: true,//商城checkbox
     checkedCarts: []
   },
   onManage() {
     this.setData({
-      isChecked: !this.data.isChecked
-    })
+      isChecked: !this.data.isChecked,
+      list: this.data.list
+    })    
+    // console.log(this.data.isChecked)
     let carts = this.data.list[0].products;
-    //如果商城选中
-    if (this.data.isChecked) {
-      //将每个商品状态改为选中
-      carts.filter(function (t) {
-        return t.isChecked = this.data.isChecked
-      })
-    }
+    let _this = this;
+    carts.forEach(function (t) {
+      t.isChecked = _this.data.isChecked;
+    })
     //将改完的状态赋值给data中的list
-    let list = this.data.list[0];
-    // list[0].products = carts
-    console.log(this.data.list[0]);
+    this.data.list[0].products = carts;
+    this.data.list[0].isChecked = this.data.isChecked;
     //重新赋值list
-    // this.setData({
-    //   isChecked: this.data.isChecked,
-    //   list: list 
-    // })    
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    var checkedCarts = [];
-    for (var i = 0; i < this.data.list.length; i++) {
-      var shopItem = this.data.list[i]
-      if (!(shopItem.products && shopItem.products.length > 0)) {
-        continue
-      }
-      var shop_id = shopItem.id;
-      var carts = [];
-      var flag = true;
-      let sum = 0;
-      for (var j = 0; j < shopItem.products.length; j++) {
-        var cartItem = shopItem.products[j];
-        carts.push(cartItem);
-        if (cartItem.isChecked) {
-          sum += cartItem.price * cartItem.num;
-          //存储已选数组
-          checkedCarts.push(cartItem.id)
-        } else {
-          flag = false
-        }
-      }
-    }    
+    this.setData({
+      isChecked: this.data.isChecked,
+      list: this.data.list
+    })
   },
   toggleCheckedCarts(e) {
     let carts = this.data.list[0].products;
     let eId = (e.target.dataset.id).toString();
-    var cart = {}
-    for (var i = 0, length = carts.length; i < length; i++) {
-      var item = carts[i];
+    let cart = {};
+    for (let i = 0, length = carts.length; i < length; i++) {
+      let item = carts[i];
       if (item.id === eId) {
         cart = item
-        cart.isChecked = !cart.isChecked
+        cart.isChecked = !cart.isChecked;
         break
-      }
+      }      
     }
-    // console.log(this.data.list)
     this.setData({
       list: this.data.list
     })
+    //实现点击单个商品切换全选按钮
+    let isCheckeds = [];
+    for (let j = 0, length = this.data.list[0].products.length; j < length; j++) {
+      let item = this.data.list[0].products[j];
+      isCheckeds.push(item.isChecked);  
+    }
+    if (isCheckeds.indexOf(false)>-1) {
+      this.setData({
+        isChecked: false
+      })
+    }else {
+      this.setData({
+        isChecked: true
+      })    
+    }
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    this.setData({
-      
-    })
+  //  console.log(this.data.list,this.data.isChecked)
+    this.calCart = this.selectComponent("#calCart")
   },
 
   /**
