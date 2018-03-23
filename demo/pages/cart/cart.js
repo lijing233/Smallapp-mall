448489487091ed3642e420fca68aeddafc915853
zhouyali <zhouyali@gomeholdings.com>
2018-03-23 17:47:44 +0800

    cart done

--- demo/pages/cart/cart.js
+++ demo/pages/cart/cart.js
@@ -15,7 +15,8 @@ Page({
     isChecked: true,//商城checkbox
     checkedCarts: [],
     number: 0,
-    id: ''
+    id: '',
+    allSum: 0
   },
   onManage() {
     this.setData({
@@ -31,6 +32,13 @@ Page({
     //将改完的状态赋值给data中的list
     this.data.list[0].products = carts;
     this.data.list[0].isChecked = this.data.isChecked;
+    if(!this.data.isChecked) {
+      this.setData({
+        allSum: 0
+      })
+    }else {
+      this.originSum()     
+    }
     //重新赋值list
     this.setData({
       isChecked: this.data.isChecked,
@@ -40,12 +48,27 @@ Page({
   toggleCheckedCarts(e) {
     let carts = this.data.list[0].products;
     let eId = (e.target.dataset.id).toString();
+    let ePrice = e.target.dataset.price;
+    let eNum = e.target.dataset.num;
     let cart = {};
+    let eItemSum = 0;
     for (let i = 0, length = carts.length; i < length; i++) {
       let item = carts[i];
       if (item.id === eId) {
-        cart = item
+        cart = item;
         cart.isChecked = !cart.isChecked;
+        eItemSum = ePrice * eNum
+        if (!cart.isChecked) {
+          let changeSum = this.data.allSum - eItemSum
+          this.setData({
+            allSum: changeSum
+          })
+        }else {
+          let changeSum = this.data.allSum + eItemSum
+          this.setData({
+            allSum: changeSum
+          })
+        }
         break
       }      
     }
@@ -53,50 +76,96 @@ Page({
       list: this.data.list
     })
     //实现点击单个商品切换全选按钮
+    this.allCheckedStatus()
+  },
+  allCheckedStatus() {
     let isCheckeds = [];
     for (let j = 0, length = this.data.list[0].products.length; j < length; j++) {
       let item = this.data.list[0].products[j];
-      isCheckeds.push(item.isChecked);  
+      isCheckeds.push(item.isChecked);
     }
-    if (isCheckeds.indexOf(false)>-1) {
+    if (isCheckeds.indexOf(false) > -1) {
       this.setData({
         isChecked: false
       })
-    }else {
+    } else {
       this.setData({
         isChecked: true
-      })    
+      })
     }
   },
   //start组件粘贴过来的
   plusNumber(e) {
-    let pn = parseInt(e.target.dataset.number);
-    let id = e.target.dataset.id;
-    console.log(id)
-    pn +=1
-    this.setData({
-      number: pn
-    })
+    this.commonFun(0, e)
   },
   minusNumber(e) {
-    console.log(this.data.number--)
-    let mn = this.data.number--
-    if (mn < 0) {
-      mn = 0
-    }
-    this.setData({
-      number: mn
-    })
+    this.commonFun(1, e)
   },
   //end组件粘贴过来的
+  commonFun(n, e) {
+    let pn = parseInt(e.target.dataset.number);
+    if (n == 0) {
+      pn += 1
+    } else {
+      pn -= 1
+      if (pn < 0) {
+        pn = 0
+      }
+    }
+    let itemId = e.target.dataset.id;
+    let isChecked = e.target.dataset.isChecked;
+    let carts = this.data.list[0].products;
+    let _this = this;
+    let allSum = 0;
+    let itemSum = 0;
+    if(n == 1) {
+      carts.forEach(function (t) {
+        if (t.id == itemId) {
+          t.isChecked = true
+          t.num = pn;
+        }
+        itemSum = t.num * t.price
+        allSum = allSum - itemSum
+      })
+    }else if(n==0){
+      carts.forEach(function (t) {
+        if (t.id == itemId) {
+          t.isChecked = true
+          t.num = pn;
+        }
+        itemSum = t.num * t.price;
+        allSum = allSum + itemSum
+      })     
+    }
+    this.allCheckedStatus()
+    //将改完的状态赋值给data中的list
+    this.data.list[0].products = carts;
+    //重新赋值list
+    this.setData({
+      list: this.data.list,
+      number: pn,
+      allSum: allSum > 0 ? allSum : allSum*(-1)
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  //  console.log(this.data.list,this.data.isChecked)
     this.calCart = this.selectComponent("#calCart")
+    this.originSum()
   },
-
+  originSum() {
+    let carts = this.data.list[0].products;
+    let allSum = 0;
+    let itemSum = 0;
+    carts.forEach(function (t) {
+      itemSum = t.num * t.price;
+      allSum = allSum + itemSum
+    })
+    this.setData({
+      allSum: allSum
+    }) 
+  },
   /**
    * 生命周期函数--监听页面显示
    */
